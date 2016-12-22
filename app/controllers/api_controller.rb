e3214fc4d8af536c99dd9e4264060c5ee5bea2e9
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-23 00:48:12 +1100

    lalala

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,26 +1,26 @@
-class ApiController < ApplicationController  
-  http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], :only => [:signup]  
-  before_filter :check_for_valid_authtoken, :except => [:signup]
+class ApiController < ApplicationController
+  http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], :only => [:signup, :signin]
+  before_filter :check_for_valid_authtoken, :except => [:signup, :signin]
   skip_before_filter :verify_authenticity_token
 
   def signup
     if request.post?
       if params && params[:full_name] && params[:email] && params[:password]
-        
-        params[:user] = Hash.new    
+
+        params[:user] = Hash.new
         params[:user][:first_name] = params[:full_name].split(" ").first
         params[:user][:last_name] = params[:full_name].split(" ").last
         params[:user][:email] = params[:email]
-        
-        begin 
+
+        begin
           decrypted_pass = AESCrypt.decrypt(params[:password], ENV["API_AUTH_PASSWORD"])
         rescue Exception => e
-          decrypted_pass = nil          
+          decrypted_pass = nil
         end
-                
-        params[:user][:password] = decrypted_pass  
+
+        params[:user][:password] = decrypted_pass
         params[:user][:verification_code] = rand_string(20)
-        
+
         user = User.new(user_params)
 
         if user.save
@@ -28,10 +28,10 @@ class ApiController < ApplicationController
         else
           error_str = ""
 
-          user.errors.each{|attr, msg|           
+          user.errors.each{|attr, msg|
             error_str += "#{attr} - #{msg},"
           }
-                    
+
           e = Error.new(:status => 400, :message => error_str)
           render :json => e.to_json, :status => 400
         end
@@ -44,24 +44,24 @@ class ApiController < ApplicationController
 
    def signin
     if request.post?
-      if params && params[:email] && params[:password]        
+      if params && params[:email] && params[:password]
         user = User.where(:email => params[:email]).first
-                      
-        if user 
-          if User.authenticate(params[:email], params[:password]) 
-                    
+
+        if user
+          if User.authenticate(params[:email], params[:password])
+
             if !user.api_authtoken || (user.api_authtoken && user.authtoken_expiry < Time.now)
               auth_token = rand_string(20)
               auth_expiry = Time.now + (24*60*60)
-          
-              user.update_attributes(:api_authtoken => auth_token, :authtoken_expiry => auth_expiry)    
-            end 
-                                   
+
+              user.update_attributes(:api_authtoken => auth_token, :authtoken_expiry => auth_expiry)
+            end
+
             render :json => user.to_json, :status => 200
           else
             e = Error.new(:status => 401, :message => "Wrong Password")
             render :json => e.to_json, :status => 401
-          end      
+          end
         else
           e = Error.new(:status => 400, :message => "No USER found by this email ID")
           render :json => e.to_json, :status => 400
@@ -82,7 +82,7 @@ class ApiController < ApplicationController
     return string
   end
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_hash, :password_salt, :verification_code, 
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_hash, :password_salt, :verification_code,
     :email_verification, :api_authtoken, :authtoken_expiry)
   end
 
