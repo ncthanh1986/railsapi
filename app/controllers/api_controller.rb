54cc8b8fdb6cb928915dbfc70b400c2b52f0326c
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-22 16:38:35 +1100

    ready to push

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,89 @@
+class ApiController < ApplicationController  
+  http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], :only => [:signup]  
+  before_filter :check_for_valid_authtoken, :except => [:signup]
+  skip_before_filter :verify_authenticity_token
+
+  def signup
+    if request.post?
+      if params && params[:full_name] && params[:email] && params[:password]
+        
+        params[:user] = Hash.new    
+        params[:user][:first_name] = params[:full_name].split(" ").first
+        params[:user][:last_name] = params[:full_name].split(" ").last
+        params[:user][:email] = params[:email]
+        
+        begin 
+          decrypted_pass = AESCrypt.decrypt(params[:password], ENV["API_AUTH_PASSWORD"])
+        rescue Exception => e
+          decrypted_pass = nil          
+        end
+                
+        params[:user][:password] = decrypted_pass  
+        params[:user][:verification_code] = rand_string(20)
+        
+        user = User.new(user_params)
+
+        if user.save
+          render :json => user.to_json, :status => 200
+        else
+          error_str = ""
+
+          user.errors.each{|attr, msg|           
+            error_str += "#{attr} - #{msg},"
+          }
+                    
+          e = Error.new(:status => 400, :message => error_str)
+          render :json => e.to_json, :status => 400
+        end
+      else
+        e = Error.new(:status => 400, :message => "required parameters are missing")
+        render :json => e.to_json, :status => 400
+      end
+    end
+  end
+
+   def signin
+    if request.post?
+      if params && params[:email] && params[:password]        
+        user = User.where(:email => params[:email]).first
+                      
+        if user 
+          if User.authenticate(params[:email], params[:password]) 
+                    
+            if !user.api_authtoken || (user.api_authtoken && user.authtoken_expiry < Time.now)
+              auth_token = rand_string(20)
+              auth_expiry = Time.now + (24*60*60)
+          
+              user.update_attributes(:api_authtoken => auth_token, :authtoken_expiry => auth_expiry)    
+            end 
+                                   
+            render :json => user.to_json, :status => 200
+          else
+            e = Error.new(:status => 401, :message => "Wrong Password")
+            render :json => e.to_json, :status => 401
+          end      
+        else
+          e = Error.new(:status => 400, :message => "No USER found by this email ID")
+          render :json => e.to_json, :status => 400
+        end
+      else
+        e = Error.new(:status => 400, :message => "required parameters are missing")
+        render :json => e.to_json, :status => 400
+      end
+    end
+  end
+
+  private
+
+  def rand_string(len)
+    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+    string  =  (0..len).map{ o[rand(o.length)]  }.join
+
+    return string
+  end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_hash, :password_salt, :verification_code, 
+    :email_verification, :api_authtoken, :authtoken_expiry)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	  resources :users
+  post 'api/signup'
+  post 'api/signin'
+
+  post 'api/upload_photo'
+  match "*path", to: "application#page_not_found", via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
